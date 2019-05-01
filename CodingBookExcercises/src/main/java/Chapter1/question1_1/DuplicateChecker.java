2f447017e919795dadb711f2e6c2844082cf984d
cjrgr <cjrgr@Christopher.home>
2019-05-01 22:37:28 +0100

    code for question 1 and mockito addded and used

--- CodingBookExcercises/src/main/java/Chapter1/question1_1/DuplicateChecker.java
+++ CodingBookExcercises/src/main/java/Chapter1/question1_1/DuplicateChecker.java
@@ -1,8 +1,30 @@
 package Chapter1.question1_1;
 
 class DuplicateChecker {
+	private CharFrequencyRecorder frequencyRecorder;
 	
-	protected DuplicateChecker() {
-		
+	protected DuplicateChecker(CharFrequencyRecorder frequencyRecorder) {
+		this.frequencyRecorder = frequencyRecorder;
 	}
+	
+	public boolean checkForDuplicates(String stringForCheck) {
+		boolean isDuplicate = false;
+		
+		char[] letters = stringForCheck.toCharArray();
+		for(char letter:letters) {
+			this.frequencyRecorder.recordOccurrence(letter);
+			if(hasDuplicateBeenFound(letter)) {
+				isDuplicate = true;
+				break;
+			}
+		}
+		
+		return isDuplicate;
+	}
+	
+	private boolean hasDuplicateBeenFound(char letter) {
+		int occurrencesSoFar = this.frequencyRecorder.getOccurences(letter);
+		return occurrencesSoFar>1;
+	}
+	
 }
--- CodingBookExcercises/src/test/java/Chapter1/question1_1/DuplicateCheckerTest.java
+++ CodingBookExcercises/src/test/java/Chapter1/question1_1/DuplicateCheckerTest.java
@@ -4,16 +4,52 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class DuplicateCheckerTest {
-
+	
+	@Mock
+	private CharFrequencyRecorder frequencyRecorder;
+	
+	@InjectMocks
+	private DuplicateChecker duplicateChecker;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		for(char characterOfAlphabet = 'a'; characterOfAlphabet <='z'; characterOfAlphabet ++ )
+        {
+			/* Has to be lenient because all letters
+			 * of alphabet get mocked for each test 
+			 * but not neccessarily used. Otherwise 
+			 * mocking code would be much longer
+			 * 
+			 */
+			Mockito.lenient()
+				   .doReturn(1,2)
+				   .when(frequencyRecorder)
+				   .getOccurences(characterOfAlphabet);
+        }
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void hasDuplicateLetters() {
+		boolean hasDuplicate;
+		String testString = "abcdddefg";
+		hasDuplicate = duplicateChecker.checkForDuplicates(testString);
+		assertTrue(hasDuplicate);
+	}
+	
+	@Test
+	void lacksDuplicateLetters() {
+		boolean hasDuplicate;
+		String testString = "abcdefg";
+		hasDuplicate = duplicateChecker.checkForDuplicates(testString);
+		assertFalse(hasDuplicate);
 	}
 
 }
